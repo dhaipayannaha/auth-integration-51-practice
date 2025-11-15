1a5fed518f982b53658a25611873e266306f5fa0
DESKTOP-U096J61\dhaip <dhaipayannahadipya@gmail.com>
2025-11-16 02:00:56 +0600

    authantication data receive from register

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,9 +1,12 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React from 'react';
+
+import React, { use } from 'react';
 import { Link } from 'react-router';
-import { auth } from '../../firebase.init';
+import { AuthContext } from '../../Context/AuthContext';
 
 const Register = () => {
+
+    const {createUser} = use(AuthContext);
+    
 
     const handleRegister = (e) => {
         e.preventDefault()
@@ -12,14 +15,21 @@ const Register = () => {
         const email = e.target.email.value;
         const password = e.target.password.value;
 
-        createUserWithEmailAndPassword(auth, email, password)
+        // createUserWithEmailAndPassword(auth, email, password)
+        // .then(result => {
+        //     console.log(result.user)
+        // })
+        // .catch(error => {
+        //     console.log(error)
+        // })
+
+        createUser(email, password)
         .then(result => {
             console.log(result.user)
         })
         .catch(error => {
             console.log(error)
         })
-
 
     }
 
--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase.init';
 
 const AuthProvider = ({ children }) => {
 
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
     const userInfo = {
-        email: 'gsfr@gmail.com'
+        createUser
     }
 
     return (
         <AuthContext value={userInfo}>
             {children}
         </AuthContext>
-        // <AuthContext value={userInfo}>
-        //     {children}
-        // </AuthContext>
     );
 };
 
